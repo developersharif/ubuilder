d1fed349f4bdb6146b68b32a4d673f711f2df70c
Sharif <developersharif@yahoo.com>
2026-05-20 18:59:21 +0600

    chore: bump version to v2.4.2

--- src/core/ubuilder.h
+++ src/core/ubuilder.h
@@ -21,7 +21,7 @@ extern "C" {
 // preprocessor, so bumping the patch level means editing only this file.
 #define UBUILDER_VERSION_MAJOR 2
 #define UBUILDER_VERSION_MINOR 4
-#define UBUILDER_VERSION_PATCH 1
+#define UBUILDER_VERSION_PATCH 2
 
 // Platform definitions
 #ifdef _WIN32
